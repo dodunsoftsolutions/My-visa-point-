c4db9bd14005f934ab332a3cc70e6ab013b8ebd9
Harsh Mangal <116105912+harsh-mangal@users.noreply.github.com>
2024-09-27 00:39:29 +0530

    changes done

--- Server/index.js
+++ Server/index.js
@@ -27,6 +27,6 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-app.get("/" , (res)=>{
-  res.send("Welcome to VISAPOINT API!")
-})
+app.get('/', (req, res) => {
+  res.send('Welcome to VisaPointAPI!');
+});
